f2a6b572d989d5dae0ef3dd4e97a3f0c02da49a8
jinhw12 <jinhw12@naver.com>
2021-11-26 00:34:48 +0900

    feat: SON-23 카카오 map api 연결

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,22 @@
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+// import React from 'react';
+import {useEffect, useRef} from 'react';
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
 function Map() {
-  return <div></div>;
+  const container = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const options = {
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+    new window.kakao.maps.Map(container.current, options);
+  }, []);
+  return <div ref={container} style={{width: '100vw', height: '100vh'}}></div>;
 }
 
 export default Map;

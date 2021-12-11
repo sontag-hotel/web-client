0699d012dd084d9ec07833d0691a6cee7a26e661
jinhw12 <jinhw12@naver.com>
2021-12-12 03:45:20 +0900

    feat: SON-53 마커 렌더 테스트

--- src/Apollo.tsx
+++ src/Apollo.tsx
@@ -1,6 +1,20 @@
-import {ApolloClient, InMemoryCache} from '@apollo/client';
+import {ApolloClient, InMemoryCache, makeVar} from '@apollo/client';
 
-//export const isLoggedInVar = makeVar(false);
+//export const isLoggedIn = makeVar(false);
+//더미데이터
+export const selectedThemeVar = makeVar([
+  {
+    title: '센터커피 서울숲점',
+    address: '서울 성동구 서울숲2길 28-11 1층',
+    location: {longitude: '37.546541849098304', latitude: '127.048963363388'},
+  },
+  {
+    title: '이월 커피로스터스',
+    address: '서울 송파구 백제고분로45길 14',
+    location: {longitude: '37.5096875273204', latitude: '127.11001081659107'},
+  },
+]);
+//
 
 export const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,26 +1,62 @@
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 // import React from 'react';
-import {useEffect, useRef} from 'react';
+import {useEffect} from 'react';
 import styled from 'styled-components';
+import markerImg from 'assets/images/marker1.png';
+import {selectedThemeVar} from 'Apollo';
+import {useReactiveVar} from '@apollo/client';
 
-declare const kakao: any;
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
 
 const SMap = styled.div`
   z-index: 1;
 `;
 
 function Map(): React.ReactElement {
-  const container = useRef<HTMLDivElement>(null);
+  const selectedTheme = useReactiveVar(selectedThemeVar);
+
+  //지도에 선택된 테마 마커 표시
   useEffect(() => {
-    const options = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667),
-      level: 3,
+    const showMarkers = () => {
+      const container = document.getElementById('kakao-map');
+      const options = {
+        center: new window.kakao.maps.LatLng(
+          37.546541849098304,
+          127.0416230371248
+        ),
+        level: 3,
+      };
+      const map = new window.kakao.maps.Map(container, options);
+      const bounds = new window.kakao.maps.LatLngBounds();
+      for (let i = 0; i < selectedTheme.length; i++) {
+        const markerImage = new window.kakao.maps.MarkerImage(
+          markerImg,
+          new window.kakao.maps.Size(24, 35)
+        );
+        const points = new window.kakao.maps.LatLng(
+          selectedTheme[i].location.longitude,
+          selectedTheme[i].location.latitude
+        );
+        const marker = new window.kakao.maps.Marker({
+          map: map,
+          position: points,
+          title: selectedTheme[i].title,
+          image: markerImage,
+        });
+        marker.setMap(map);
+        bounds.extend(points);
+        map.setBounds(bounds);
+      }
     };
-    new kakao.maps.Map(container.current, options);
-  }, []);
+    showMarkers();
+  }, [selectedTheme]);
   return (
     <SMap>
-      <div ref={container} style={{width: '100vw', height: '100vh'}}></div>;
+      <div id="kakao-map" style={{width: '100vw', height: '100vh'}}></div>;
     </SMap>
   );
 }
